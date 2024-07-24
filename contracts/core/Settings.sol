e4e3592dd9cad3ed8cf502ad5964cac6008ca7bf
Emre Kaya <emrekaya75@outlook.com>
2024-07-25 01:17:14 +0300

    some libraries and interfaces are added

--- contracts/core/Settings.sol
+++ contracts/core/Settings.sol
@@ -1,6 +1,12 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {Ownable} from "../access/Ownable.sol";
+import {Pausable} from "../utils/Pausable.sol";
+import {ReentrancyGuard} from "../utils/ReentrancyGuard.sol";
+
 contract Settings{
-    // Wait my logic weapon
+    
+
+
 }
