479125c81f3a4dedbe82a2cea7546e43d284c8d8
Emre Kaya <emrekaya75@outlook.com>
2024-07-26 02:07:45 +0300

    settings contract is done

--- contracts/LendmeFi.sol
+++ contracts/LendmeFi.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {ERC721Holder} from "./core/ERC721Holder.sol";
+import {IERC20} from "./interfaces/IERC20.sol";
+import {IERC721} from "./interfaces/IERC721.sol";
+import {SafeERC20} from "./library/SafeERC20.sol";
+import {Settings} from "./core/Settings.sol";
 
-contract LendmeFi{
-    // Wait my logic weapon
+
+
+contract LendmeFi is ERC721Holder, Settings {
+    using SafeERC20 for IERC20;
+
+    constructor(address _weth, address _usdc) Settings( _weth, _usdc) {}
 }
--- contracts/core/Settings.sol
+++ contracts/core/Settings.sol
@@ -5,8 +5,43 @@ import {Ownable} from "../access/Ownable.sol";
 import {Pausable} from "../utils/Pausable.sol";
 import {ReentrancyGuard} from "../utils/ReentrancyGuard.sol";
 
-contract Settings{
-    
+contract Settings is Ownable, Pausable, ReentrancyGuard {
+    mapping(address => bool) public erc20whitelist;
+    mapping(address => bool) public erc721whitelist;
 
+    uint256 public lendmefiFee = 200; // 2% fee for LendmeFi platform.
 
+    event LendmefiFeeChanged(uint256 newFee);
+
+    constructor(address _weth, address _usdc) Ownable(msg.sender) {
+        erc20whitelist[_weth] = true;
+        erc20whitelist[_usdc] = true;
+    }
+
+    function setLendmefiFee(uint256 _fee) external onlyOwner {
+        lendmefiFee = _fee;
+        emit LendmefiFeeChanged(_fee);
+    }
+
+    function setERC20Whitelist(
+        address _token,
+        bool _status
+    ) external onlyOwner {
+        erc20whitelist[_token] = _status;
+    }
+
+    function setERC721Whitelist(
+        address _token,
+        bool _status
+    ) external onlyOwner {
+        erc721whitelist[_token] = _status;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
